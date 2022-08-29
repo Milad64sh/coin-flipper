39957085920a32383486ff18cc9289722982d228
Milad64sh <m.shalikarian@gmail.com>
2022-08-29 12:02:47 -0700

    box colors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React, { Component } from 'react';
 import './App.css';
+import BoxContainer from './BoxContainer';
 
-function App() {
-  return <div className='App'></div>;
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <BoxContainer />
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/BoxContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Box from './Box';
+import './BoxContainer.css';
+
+export default class BoxContainer extends Component {
+  static defaultProps = {
+    numBoxes: 18,
+  };
+  render() {
+    const boxes = Array.from({ length: this.props.numBoxes }).map(() => (
+      <Box />
+    ));
+    return <div className='BoxContainer'>{boxes}</div>;
+  }
+}
